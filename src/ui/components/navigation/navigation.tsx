07b440cbe5d3e579ece03e997fb079b3b0018194
zoubair_djebah <zoubairdjebah.dev@gmail.com>
2023-12-10 20:26:59 +0100

    feature(navigation): gerer les routes dans notre application

--- src/ui/components/navigation/navigation.tsx
+++ src/ui/components/navigation/navigation.tsx
@@ -2,6 +2,7 @@ import Typography from "@/ui/design-system/typography/typography";
 import Container from "@/ui/components/container/container";
 import Logo from "@/ui/design-system/logo/logo";
 import Button from "@/ui/design-system/button/button";
+import Link from "next/link";
 
 interface NavigationProps {}
 
@@ -9,29 +10,36 @@ const Navigation = ({}: NavigationProps) => {
   return (
     <div className="border-b-2 border-gray-400">
       <Container className="flex items-center justify-between py-1.5 gap-7">
-        <div className="flex items-center gap-2.5">
-          <Logo size="small" />
-          <div className="flex flex-col">
-            <div className="text-gray font-extrabold text-[24px]">
-              Coders Monkeys
+        <Link href="/">
+          <div className="flex items-center gap-2.5">
+            <Logo size="small" />
+            <div className="flex flex-col">
+              <div className="text-gray font-extrabold text-[24px]">
+                Coders Monkeys
+              </div>
+              <Typography variant="caption4" component="span" theme="gray">
+                Trouve de l’inspiration & reçois des feedbacks !
+              </Typography>
             </div>
-            <Typography variant="caption4" component="span" theme="gray">
-              Trouve de l’inspiration & reçois des feedbacks !
-            </Typography>
           </div>
-        </div>
+        </Link>
         <div className="flex items-center gap-7">
-          <Typography variant="caption3" component="div" className="flex flex-center gap-7">
-            <span>Projets</span>
-            <span>Formations</span>
-            <span>Contsact</span>
+          <Typography
+            variant="caption3"
+            component="div"
+            className="flex flex-center gap-7"
+          >
+            <Link href="/projets">Projets</Link>
+            <Link href="/formations">Formations</Link>
+            <Link href="/contact">Contact</Link>
           </Typography>
           <div className="flex items-center gap-2">
-            <Button size="small" variant="secondary">Connexion</Button>
-            <Button size="small">Rejoindre</Button>
+            <Button size="small" variant="secondary">
+              Se connecter
+            </Button>
+            <Button size="small">Nous rejoindre</Button>
           </div>
         </div>
-
       </Container>
     </div>
   );
